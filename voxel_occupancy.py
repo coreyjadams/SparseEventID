ba4dcb9229c281ef7b4fd3c5736074bac662f311
Corey Adams <corey.adams@anl.gov>
2019-08-01 09:39:17 -0500

    Add script to check voxel occupancy levels

--- voxel_occupancy.py
+++ voxel_occupancy.py
@@ -1,43 +1,67 @@
-from ROOT import larcv
+from larcv import larcv
 import numpy
 
-io = larcv.IOManager()
-io.add_in_file("/lus/theta-fs0/projects/datascience/cadams/wire_pixel_fullres_files/sbnd_dl_eventID_val.root")
-io.initialize()
 
-voxel_counts = numpy.zeros((io.get_n_entries(), 3))
-voxel_counts3d = numpy.zeros((io.get_n_entries(), 1))
 
-for i in range(io.get_n_entries()):
-    io.read_entry(i)
-    image = io.get_data("sparse2d", "sbndwire")
-    for plane in [0,1,2]:
-        voxel_counts[i][plane] = image.as_vector()[plane].size()
-        meta = image.as_vector()[plane].meta()
-    image3d = io.get_data("sparse3d", "sbndvoxels")
-    voxel_counts3d[i] = image3d.as_vector().size()
 
-    if i % 100 == 0:
-        print("On entry ", i, " of ", io.get_n_entries())
+def count_2d(file_name, product, producer):
+    io = larcv.IOManager()
+    io.add_in_file(file_name)
+    io.initialize()
+    voxel_counts = numpy.zeros((io.get_n_entries(), 3))
 
-    # if i > 100:
-    #     break
+    for i in range(io.get_n_entries()):
+        io.read_entry(i)
+        image = larcv.EventSparseTensor2D.to_sparse_tensor(io.get_data("sparse2d", "sbndvoxels"))
+        for plane in [0,1,2]:
+            voxel_counts[i][plane] = image.as_vector()[plane].size()
+            meta = image.as_vector()[plane].meta()
+        # image3d = io.get_data("sparse3d", "sbndvoxels")
+        # voxel_counts3d[i] = image3d.as_vector().size()
 
-print ("Average Voxel Occupation: ")
-for p in [0,1,2]:
-    print("  {p}: {av:.2f} +/- {rms:.2f} ({max} max)".format(
-        p   = p, 
-        av  = numpy.mean(voxel_counts[:,p]), 
-        rms = numpy.std(voxel_counts[:,p]), 
-        max = numpy.max(voxel_counts[:,p])
+        if i % 100 == 0:
+            print("On entry ", i, " of ", io.get_n_entries())
+
+        # if i > 100:
+        #     break
+
+    print ("Average Voxel Occupation: ")
+    for p in [0,1,2]:
+        print("  {p}: {av:.2f} +/- {rms:.2f} ({max} max)".format(
+            p   = p, 
+            av  = numpy.mean(voxel_counts[:,p]), 
+            rms = numpy.std(voxel_counts[:,p]), 
+            max = numpy.max(voxel_counts[:,p])
+            )
+        )
+
+
+def count_3d(file_name, product, producer):
+    io = larcv.IOManager()
+    io.add_in_file(file_name)
+    io.initialize()
+    voxel_counts3d = numpy.zeros((io.get_n_entries(), 1))
+    for i in range(io.get_n_entries()):
+        io.read_entry(i)
+        image3d = larcv.EventSparseTensor3D.to_sparse_tensor(io.get_data("sparse3d", "sbndvoxels"))
+        voxel_counts3d[i] = image3d.as_vector()[0].size()
+
+        if i % 100 == 0:
+            print("On entry ", i, " of ", io.get_n_entries())
+
+        # if i > 100:
+        #     break
+    print(" 3D: {av:.2f} +/- {rms:.2f} ({max} max)".format(
+        av  = numpy.mean(voxel_counts3d[:]), 
+        rms = numpy.std(voxel_counts3d[:]), 
+        max = numpy.max(voxel_counts3d[:])
         )
     )
-print("  {p}: {av:.2f} +/- {rms:.2f} ({max} max)".format(
-    p   = p, 
-    av  = numpy.mean(voxel_counts3d[:]), 
-    rms = numpy.std(voxel_counts3d[:]), 
-    max = numpy.max(voxel_counts3d[:])
-    )
-)
-numpy.save("2d_counts.npz",voxel_counts)
-numpy.save("3d_counts.npz",voxel_counts3d)
+
+
+
+if __name__ == '__main__':
+    # count_2d("data_files/out_2d_train_rand.h5", "sparse2d", "sbndvoxels")
+    count_3d("data_files/out_3d_train_rand.h5", "sparse3d", "sbndvoxels")
+
+
