5a16ce370edd2bfdc79978188d30f820473e018a
Marco Del Tutto <marco.deltutto@gmail.com>
2019-11-04 09:04:51 -0500

    Right name and types to store in output inference file.

--- src/utils/io_templates.py
+++ src/utils/io_templates.py
@@ -88,8 +88,10 @@ def output_io(input_file, output_file):
     # These lines slim down the output file.
     # Without them, 25 output events is 2.8M and takes 38s
     # With the, 25 output events is 119K and takes 36s
-    config.set_param("ReadOnlyType", "[\"particle\",\"particle\",\"particle\",\"particle\",\"particle\",\"particle\",\"cluster2d\",\"cluster2d\",\"cluster3d\",\"cluster3d\"]")  
-    config.set_param("ReadOnlyName", "[\"cpiID\",\"neutID\",\"npiID\",\"protID\",\"all\",\"sbndsegmerged\",\"sbndsegmerged\",\"sbndneutrino\",\"sbndsegmerged\",\"sbndneutrino\"]")  
+    # config.set_param("ReadOnlyType", "[\"particle\",\"particle\",\"particle\",\"particle\",\"particle\",\"particle\",\"particle\",\"cluster2d\",\"cluster2d\",\"cluster3d\",\"cluster3d\"]")  
+    # config.set_param("ReadOnlyName", "[\"neutrino\",\"cpiID\",\"neutID\",\"npiID\",\"protID\",\"all\",\"duneseg\",\"duneseg\",\"segment\",\"duneseg\",\"segment\"]")  
+    config.set_param("ReadOnlyType", "[\"particle\",\"particle\",\"particle\",\"particle\",\"particle\",\"particle\",\"particle\",\"cluster2d\"]")  
+    config.set_param("ReadOnlyName", "[\"neutrino\",\"cpiID\",\"neutID\",\"npiID\",\"protID\",\"all\",\"segment\",\"duneseg\"]")  
 
     return config
 
@@ -118,7 +120,7 @@ def gen_sparse3d_data_filler(name, producer, max_voxels):
     proc.set_param("IncludeValues",     "true")
     proc.set_param("MaxVoxels",         max_voxels)
     proc.set_param("UnfilledVoxelValue","-999")
-    proc.set_param("Augment",           "true")
+    proc.set_param("Augment",           "false")
 
     return proc
 
