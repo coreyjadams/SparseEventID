a044b8f2d334ed8b559dddfdee749278b4f2a134
Marco Del Tutto <marco.deltutto@gmail.com>
2019-11-05 15:59:59 -0600

    Latest analysis scripts

--- analysis/dune/tools.py
+++ analysis/dune/tools.py
@@ -11,6 +11,28 @@ def pot_scale_factor(this_pot, target = 1e20):
 prop_cycle = plt.rcParams['axes.prop_cycle']
 colors = prop_cycle.by_key()['color']
 
+def lighten_color(color, amount=0.5):
+    """
+    Lightens the given color by multiplying (1-luminosity) by the given amount.
+    Input can be matplotlib color string, hex string, or RGB tuple.
+
+    Examples:
+    >> lighten_color('g', 0.3)
+    >> lighten_color('#F034A3', 0.6)
+    >> lighten_color((.3,.55,.1), 0.5)
+    """
+    import matplotlib.colors as mc
+    import colorsys
+    try:
+        c = mc.cnames[color]
+    except:
+        c = color
+    c = colorsys.rgb_to_hls(*mc.to_rgb(c))
+    return colorsys.hls_to_rgb(c[0], 1 - amount * (1 - c[1]), c[2])
+
+prop_cycle = plt.rcParams['axes.prop_cycle']
+colors = prop_cycle.by_key()['color']
+
 def efficiency(num, den, num_w=None, den_w=None, n_bins=10, limits=None, conf_level=None):
     '''
     Calculates the efficiency given two populations: one containig 
